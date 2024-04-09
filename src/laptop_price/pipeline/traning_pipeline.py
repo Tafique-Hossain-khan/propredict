c5890fa703866b219a62fce1573d31efefb26e6a
Tafique <tafiquehossain2003@gmail.com>
2024-04-09 12:39:02 +0530

    traning pipeline

--- src/laptop_price/pipeline/traning_pipeline.py
+++ src/laptop_price/pipeline/traning_pipeline.py
@@ -10,8 +10,41 @@ from src.exception import CustomException
 import os
 import sys
 
-if __name__ == "__main__":
+class TrainLaptop:
+    def output(self,Company,	TypeName,	Ram,	Weight,	Touchscreen,	Ips,	ppi,	Cpu_brand,	HDD,	SSD,	Gpu_brand,	os):
+        try:
+            di = DataInjection()
+            raw_data_path,train_data_path,test_data_path = di.initai_data_injection()
 
+
+            dt = DataTransformation()
+            #obj_path = dt.get_data_transformation_obj()
+            train_data ,test_data =dt.initiate_data_transformation(train_data_path,test_data_path)
+
+            mr = ModelTraner()
+            mr.model_traner(train_data,test_data)
+
+
+
+            #ci = CustomInput('Apple',	'Ultrabook',	8,	1.37,	0,	1	,226.983005,	'Intel Core i5',	0,	128,	'Intel',	'Mac')
+            ci = CustomInput(Company,	TypeName,	Ram,	Weight,	Touchscreen,	Ips,	ppi,	Cpu_brand,	HDD,	SSD,	Gpu_brand,	os)
+            df = ci.custom_dataset()
+            logging.info(df)
+
+            pred = PredictPipeline()
+            prediction = pred.predict(df)
+            return prediction
+        
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ == "__main__":
+    obj = TrainLaptop()
+    ans = obj.output('Apple',	'Ultrabook',	8,	1.37,	0,	1	,226.983005,	'Intel Core i5',	0,	128,	'Intel',	'Mac')
+    logging.info(ans)
+    '''
     try:
         di = DataInjection()
         raw_data_path,train_data_path,test_data_path = di.initai_data_injection()
@@ -36,7 +69,7 @@ if __name__ == "__main__":
 
     except Exception as e:
         raise CustomException(e,sys)
-
+    '''
 
 
     
--- src/phone_price/pipeline/traning_pipeline.py
+++ src/phone_price/pipeline/traning_pipeline.py
@@ -9,9 +9,59 @@ from src.logger import logging
 from src.exception import CustomException
 import os
 import sys
+
+class Traning:
+    '''
+    def __init__(self,dispaly_res_type	,os,	processor_brand,	connectivity_feature,	ram,	internal_storgar	
+                ,mobile_brand,	dispaly_size_in_inches,	back_camera,	front_camera,	Battery_Category) -> None:
+        self.dispaly_res_type = dispaly_res_type
+        self.os = os	
+        self.processor_brand = processor_brand
+        self.connectivity_feature = connectivity_feature
+        self.ram = ram
+        self.internal_storgar = internal_storgar
+        self.mobile_brand = mobile_brand
+        self.dispaly_size_in_inches = dispaly_size_in_inches
+        self.back_camera = back_camera
+        self.front_camera = front_camera
+        self.Battery_Category = Battery_Category
+        '''
+    def output(self,dispaly_res_type	,os,	processor_brand,	connectivity_feature,	ram,	internal_storgar	
+                ,mobile_brand,	dispaly_size_in_inches,	back_camera,	front_camera,	Battery_Category):
+        try:
+            di = DataInjection()
+            train_data_path,test_data_path = di.get_data()
+
+
+            dt = DataTransformation()
+            #obj_path = dt.get_data_transformation_obj()
+            train_data ,test_data =dt.initiate_data_transformation(train_data_path,test_data_path)
+
+            mr = ModelTraner()
+            mr.model_traner(train_data,test_data)
+
+
+
+            #ci = CustomInput('Full HD+',	'Android 11','Snapdragon',	4,	6	,128	,'vivo'	,'6',	48.0	,16.0,	'4000-5000')
+            ci = CustomInput(dispaly_res_type	,os,	processor_brand,	connectivity_feature,	ram,	internal_storgar	
+                ,mobile_brand,	dispaly_size_in_inches,	back_camera,	front_camera,	Battery_Category)
+            df = ci.custome_dataset()
+            logging.info(df)
+
+            pred = PredictPipeline()
+            prediction = pred.predict(df)
+            return prediction
+        
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
 if __name__ == "__main__":
 
-    try:
+    obj = Traning()
+    ans = obj.output('Full HD+',	'Android 11','Snapdragon',	4,	6	,128	,'vivo'	,'6',	48.0	,16.0,	'4000-5000')
+    logging.info(ans)
+    '''try:
         di = DataInjection()
         train_data_path,test_data_path = di.get_data()
 
@@ -36,6 +86,6 @@ if __name__ == "__main__":
     except Exception as e:
         raise CustomException(e,sys)
 
-
+'''
 
     
